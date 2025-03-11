3572170cee579bd878361aabf8288c49dfd3ecfa
CandraDinataa <168393059+CandraDinataa@users.noreply.github.com>
2025-03-11 13:38:04 +0700

    Update HaloGeneric.cs

--- 05_Generics/TPMODUL5_2311104061/HaloGeneric.cs
+++ 05_Generics/TPMODUL5_2311104061/HaloGeneric.cs
@@ -13,6 +13,6 @@ class Program
     static void Main()
     {
         HaloGeneric halo = new HaloGeneric();
-        halo.SapaUser("NamaPanggilan"); // Ganti dengan nama praktikan
+        halo.SapaUser("can");
     }
 }
