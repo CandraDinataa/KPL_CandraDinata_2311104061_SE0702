e25685446a485a49a007300cd894af7b41863f7b
Kinn <candradinata956@gmail.com>
2025-03-11 15:25:18 +0700

    menambahkan implementasi kalkulator

---
+++ 03_GUI_Builder_dan_GitHub/JURNALMODUL3_2311104061/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JURNALMODUL3_2311104061
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
