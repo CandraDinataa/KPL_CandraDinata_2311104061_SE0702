14afad7227fa845640b8e18870a8e4ac0fc72e46
Kinn <candradinata956@gmail.com>
2025-03-16 10:06:49 +0700

    menambahkan class SayaTubeVideo

---
+++ 06_Design_by_Contract_dan_Defensive_Programming/TPMODUL6_2311104061/SayaTubeVideo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = random.Next(10000, 99999);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine($"ID: {id}");
+        Console.WriteLine($"Title: {title}");
+        Console.WriteLine($"Play Count: {playCount}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Candra Dinata]");
+        video.PrintVideoDetails();
+    }
+}
