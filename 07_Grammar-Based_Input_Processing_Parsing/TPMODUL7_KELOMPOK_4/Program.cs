2d122b64580426694579ec8dafdba98b806fd7e1
Kinn <candradinata956@gmail.com>
2025-04-10 21:07:34 +0700

    TP Kelompok 4

---
+++ 07_Grammar-Based_Input_Processing_Parsing/TPMODUL7_KELOMPOK_4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("=== JSON 1 ===");
+        DataMahasiswa2311104061.ReadJSON();
+
+        Console.WriteLine("\n=== JSON 2 ===");
+        KuliahMahasiswa2311104061.ReadJSON();
+
+        Console.WriteLine("\nTekan Enter untuk keluar...");
+        Console.ReadLine();
+    }
+}
