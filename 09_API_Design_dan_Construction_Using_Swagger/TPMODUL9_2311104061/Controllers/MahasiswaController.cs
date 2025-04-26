e3d1b6bc056094cfba78fb88c561644545c5408c
Kinn <candradinata956@gmail.com>
2025-04-26 13:56:03 +0700

    menambahkan API mahasiswa

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TPMODUL9_2311104061/Controllers/MahasiswaController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TPMODUL9_2311104061.Models;
+using System.Collections.Generic;
+
+namespace tpmodul9_2311104061.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Candra", Nim = "2311104061" },
+            new Mahasiswa { Nama = "Dobleh", Nim = "2311104098" },
+            new Mahasiswa { Nama = "Jamal", Nim = "2311104099" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> GetAll()
+        {
+            return daftarMahasiswa;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+            return daftarMahasiswa[index];
+        }
+
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok();
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+            daftarMahasiswa.RemoveAt(index);
+            return Ok();
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/TPMODUL9_2311104061/Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace TPMODUL9_2311104061.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
