4b71e958df0d62b069f080cee3b5fa92f248455f
Kinn <candradinata956@gmail.com>
2025-05-04 14:36:14 +0700

    Class Library

---
+++ 10_Library_Construction/TPMODUL10_2311104061/AljabarApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Contoh input untuk persamaan kuadrat x^2 - 3x - 10
+            double[] kuadrat = { 1, -3, -10 };
+            double[] akar = Aljabar.AkarPersamaanKuadrat(kuadrat);
+            Console.WriteLine("Akar Persamaan Kuadrat dari (1, -3, -10):");
+      
+            Console.WriteLine($"x1 = {akar[0]}");
+            Console.WriteLine($"x2 = {akar[1]}");
+
+            // Contoh input untuk (2x - 3)^2
+            double[] linear = { 2, -3 };
+            double[] hasilKuadrat = Aljabar.HasilKuadrat(linear);
+            Console.WriteLine("\nHasil kuadrat dari (2x - 3):");
+            Console.WriteLine($"{hasilKuadrat[0]}, {hasilKuadrat[1]}, {hasilKuadrat[2]}");
+        }
+    }
+}
---
+++ 10_Library_Construction/TPMODUL10_2311104061/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        // Fungsi untuk mencari akar-akar dari persamaan kuadrat ax^2 + bx + c = 0
+        public static double[] AkarPersamaanKuadrat(double[] koef)
+        {
+            double a = koef[0];
+            double b = koef[1];
+            double c = koef[2];
+
+            double diskriminan = b * b - 4 * a * c;
+            double akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        // Fungsi untuk mengkuadratkan bentuk (ax + b)^2 menjadi [a^2, 2ab, b^2]
+        public static double[] HasilKuadrat(double[] linear)
+        {
+            double a = linear[0];
+            double b = linear[1];
+
+            double a2 = a * a;
+            double ab2 = 2 * a * b; // Term ini akan menjadi negatif jika b negatif
+            double b2 = b * b;
+
+            return new double[] { a2, ab2, b2 };
+        }
+    }
+}
