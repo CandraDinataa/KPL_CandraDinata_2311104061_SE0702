67934e02e46b5633e5c17ee0fa8ca459abeb7cc7
Kinn <candradinata956@gmail.com>
2025-05-08 13:54:24 +0700

    Jurnal Modul 10

---
+++ 10_Library_Construction/JURNALMODUL10_2311104061/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int KPK(int a, int b)
+        {
+            return a * b / FPB(a, b);
+        }
+
+        public string Turunan(int[] koefisien)
+        {
+            List<string> hasil = new List<string>();
+            int derajat = koefisien.Length - 1;
+            for (int i = 0; i < koefisien.Length - 1; i++)
+            {
+                int coef = koefisien[i] * (derajat - i);
+                if (coef == 0) continue;
+
+                string suku = coef.ToString();
+                if (derajat - i - 1 > 0)
+                {
+                    suku += "x";
+                    if (derajat - i - 1 > 1)
+                        suku += (derajat - i - 1).ToString();
+                }
+                hasil.Add(suku);
+            }
+            return string.Join(" + ", hasil).Replace("+ -", "- ");
+        }
+
+        public string Integral(int[] koefisien)
+        {
+            List<string> hasil = new List<string>();
+            int derajat = koefisien.Length - 1;
+
+            for (int i = 0; i < koefisien.Length; i++)
+            {
+                int pangkatBaru = derajat - i + 1;
+                double nilai = (double)koefisien[i] / pangkatBaru;
+
+                if (nilai == 0) continue;
+
+                string suku = "";
+                if (nilai == 1)
+                    suku = "";
+                else if (nilai == -1)
+                    suku = "-";
+                else
+                    suku = nilai % 1 == 0 ? ((int)nilai).ToString() : nilai.ToString("0.##");
+
+                suku += "x";
+                if (pangkatBaru > 1)
+                    suku += pangkatBaru.ToString();
+
+                hasil.Add(suku);
+            }
+
+            hasil.Add("C");
+            return string.Join(" + ", hasil).Replace("+ -", "- ");
+        }
+    }
+}
---
+++ 10_Library_Construction/JURNALMODUL10_2311104061/Modul10App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MatematikaLibraries;
+
+namespace Modul10App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematika mtk = new Matematika();
+
+            Console.WriteLine("FPB(60, 45) = " + mtk.FPB(60, 45));
+            Console.WriteLine("KPK(12, 8) = " + mtk.KPK(12, 8));
+
+            int[] turunanInput = { 1, 4, -12, 9 };
+            Console.WriteLine("Turunan dari x^3 + 4x^2 -12x + 9:");
+            Console.WriteLine(mtk.Turunan(turunanInput));
+
+            int[] integralInput = { 4, 6, -12, 9 };
+            Console.WriteLine("Integral dari 4x^3 + 6x^2 -12x + 9:");
+            Console.WriteLine(mtk.Integral(integralInput));
+        }
+    }
+}
