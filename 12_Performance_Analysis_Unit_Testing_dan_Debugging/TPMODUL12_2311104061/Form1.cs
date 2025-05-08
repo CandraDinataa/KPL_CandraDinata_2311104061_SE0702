6b65722ec50854417f76b907c07ba315074a9b31
Kinn <candradinata956@gmail.com>
2025-05-08 15:09:47 +0700

    TP Modul 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/TPMODUL12_2311104061/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TPMODUL12_2311104061
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Mengambil input dari textBox1
+            int input;
+            // Mengecek apakah input bisa dikonversi menjadi integer
+            if (int.TryParse(textBox1.Text, out input))
+            {
+                // Menampilkan hasil pengecekan di label1
+                label1.Text = CariTandaBilangan(input);
+            }
+            else
+            {
+                // Menampilkan pesan jika input tidak valid
+                label1.Text = "Input tidak valid";
+            }
+
+        }
+ public string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+    }
+}
+    
