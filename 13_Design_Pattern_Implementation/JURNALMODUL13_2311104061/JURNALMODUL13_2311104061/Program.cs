6260aa6905f1d5619bc9b295189c2c5f7e9c1540
Kinn <candradinata956@gmail.com>
2025-05-22 13:23:18 +0700

    Design Pattern Singleton

---
+++ 13_Design_Pattern_Implementation/JURNALMODUL13_2311104061/JURNALMODUL13_2311104061/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        // A & B
+        var data1 = PusatDataSingleton.GetDataSingleton();
+        var data2 = PusatDataSingleton.GetDataSingleton();
+
+        // C - Tambah anggota kelompok dan asisten
+        data1.AddSebuahData("Candra");
+        data1.AddSebuahData("Rafi");
+        data1.AddSebuahData("Asisten: Andi");
+
+        // D - Print dari data2
+        Console.WriteLine("Data dari data2:");
+        data2.PrintSemuaData();
+
+        // E - Hapus nama asisten
+        data2.HapusSebuahData(2); // Index ke-2 adalah "Asisten: Andi"
+
+        // F - Print ulang dari data1 (cek apakah asisten terhapus)
+        Console.WriteLine("\nSetelah penghapusan, data dari data1:");
+        data1.PrintSemuaData();
+
+        // G - Print jumlah data
+        Console.WriteLine($"\nJumlah elemen data1: {data1.GetSemuaData().Count}");
+        Console.WriteLine($"Jumlah elemen data2: {data2.GetSemuaData().Count}");
+    }
+}
---
+++ 13_Design_Pattern_Implementation/JURNALMODUL13_2311104061/JURNALMODUL13_2311104061/PusatDataSingleton.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class PusatDataSingleton
+{
+    private static PusatDataSingleton _instance;
+    public List<string> DataTersimpan { get; set; }
+
+    // Private constructor
+    private PusatDataSingleton()
+    {
+        DataTersimpan = new List<string>();
+    }
+
+    // Method untuk mendapatkan instance (singleton)
+    public static PusatDataSingleton GetDataSingleton()
+    {
+        if (_instance == null)
+        {
+            _instance = new PusatDataSingleton();
+        }
+        return _instance;
+    }
+
+    public List<string> GetSemuaData()
+    {
+        return DataTersimpan;
+    }
+
+    public void PrintSemuaData()
+    {
+        foreach (string data in DataTersimpan)
+        {
+            Console.WriteLine(data);
+        }
+    }
+
+    public void AddSebuahData(string input)
+    {
+        DataTersimpan.Add(input);
+    }
+
+    public void HapusSebuahData(int index)
+    {
+        if (index >= 0 && index < DataTersimpan.Count)
+        {
+            DataTersimpan.RemoveAt(index);
+        }
+        else
+        {
+            Console.WriteLine("Index tidak valid.");
+        }
+    }
+}
